e594c536e7697f244a15cfd76db40c55fd025f78
Kadambari Yewale <kadambari9905@gmail.com>
2026-08-10 00:49:16 +0530

    Add: valid-sudoku - submission-1

---
+++ Data Structures & Algorithms/valid-sudoku/submission-1.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        // brute force - o(n2) o(n)
+        // check every row
+        for (int row = 0; row < 9; row++){
+            unordered_set<char> seen;
+            for (int i = 0; i < 9; i++){
+                if (board[row][i] == '.') continue;
+                // if duplicate is found 
+                if (seen.count(board[row][i])) return false;
+                seen.insert(board[row][i]);
+            }
+        }
+        // check every column
+        for (int col = 0; col < 9; col++){
+            unordered_set<char> seen;
+            for (int i = 0; i < 9; i++){
+                if (board[i][col] == '.') continue;
+                // if duplicate found
+                if (seen.count(board[i][col])) return false;
+                seen.insert(board[i][col]);
+            }
+        }
+        // check the 3x3 squares
+        for (int square = 0; square < 9; square++){
+            unordered_set<char> seen;
+            // for each row in square
+            for (int i = 0; i < 3; i++){
+                // for each column in square
+                for (int j = 0; j < 3; j++){
+                    int row = (square / 3) * 3 + i;
+                    int col = (square % 3) * 3 + j;
+                    if (board[row][col] == '.') continue;
+                    // if duplicate found
+                    if (seen.count(board[row][col])) return false;
+                    seen.insert(board[row][col]);
+                }
+            }
+        }
+        return true;
+    }
+};
